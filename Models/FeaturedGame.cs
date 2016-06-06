0add75dadc8a87326f187f9aa8162557e01c37f1
Burak Kaan Köse <bkaankose@outlook.com>
2016-06-07 01:57:21 +0300

    Basic models are implemented for Games and UI.

---
+++ Models/FeaturedGame.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Stream_UWP.Models
+{
+    public class FeaturedGame
+    {
+        [JsonProperty("viewers")]
+        public int ViewerCount { get; set; }
+        [JsonProperty("channels")]
+        public int ChannelCount { get; set; }
+        [JsonProperty("game")]
+        public Game GameObject { get; set; }
+    }
+}
---
+++ Models/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Stream_UWP.Models
+{
+    public class Game
+    {
+        public string name { get; set; }
+        public int _id { get; set; }
+        public int giantbomb_id { get; set; }
+        public Logo logo { get; set; }
+    }
+}
