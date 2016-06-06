87772cbd8f5232b9781b5f6afbeddf974481c683
Burak Kaan Köse <bkaankose@outlook.com>
2016-06-07 01:55:33 +0300

    PageDialogService implemented for usability.

---
+++ Interfaces/IPageDialogService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Stream_UWP.Interfaces
+{
+    public interface IPageDialogService
+    {
+        Task DisplayAlertAsync(string message, string title = "");
+        Task<bool> DisplayConfirmationDialogAsync(string question,string title = "");
+    }
+}
---
+++ Services/PageDialogService.cs
@@ -0,0 +1,40 @@
+﻿using Simple_Stream_UWP.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace Simple_Stream_UWP.Services
+{
+    public class PageDialogService : IPageDialogService
+    {
+        /// <summary>
+        /// Basic MessageDialog displayer.
+        /// </summary>
+        /// <param name="message">Message parameter of MessageDialog</param>
+        /// <param name="title">Title parameter of MessageDialog. (Optional) </param>
+        /// <returns></returns>
+        public async Task DisplayAlertAsync(string message, string title = "")
+        {
+            await new MessageDialog(message, title).ShowAsync();
+        }
+
+        /// <summary>
+        /// Returns the boolean result depending on the choice that user choose.
+        /// </summary>
+        /// <param name="question">Question that will be asked to user in the MessageDialog.</param>
+        /// <param name="title">Simple MessageDialog title. (Optional)</param>
+        /// <returns></returns>
+        public async Task<bool> DisplayConfirmationDialogAsync(string question, string title = "")
+        {
+            var questionDialog = new MessageDialog(question);
+            questionDialog.Commands.Add(new UICommand() { Id = 0, Label = "Evet" });
+            questionDialog.Commands.Add(new UICommand() { Id = 1, Label = "Hayır" });
+            
+            var result = await questionDialog.ShowAsync();
+            return result != null ? (Int32.Parse(result.Id.ToString()) == 0 ? true : false) : false;
+        }
+    }
+}
