631d906af4e069dc14c549f87f487bc7a7bc2b01
Burak Kaan Köse <bkaankose@outlook.com>
2016-06-07 02:10:04 +0300

    Exit comfirmation on MainPage support and some trash code for test purposes.

--- ViewModels/MainPageViewModel.cs
+++ ViewModels/MainPageViewModel.cs
@@ -1,17 +1,45 @@
-﻿using Simple_Stream_UWP.Base;
+﻿using Prism.Commands;
+using Prism.Windows.AppModel;
+using Prism.Windows.Navigation;
+using Simple_Stream_UWP.Base;
+using Simple_Stream_UWP.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 
 namespace Simple_Stream_UWP.ViewModels
 {
     public class MainPageViewModel : ViewModel
     {
-        public MainPageViewModel()
-        {
+        #region Services & Repositories
 
+        private readonly ITwitchRepository _twitchRepository;
+        private readonly IPageDialogService _dialogService;
+
+        public MainPageViewModel(IDeviceGestureService gestureService,INavigationService navigationService, ITwitchRepository twitchRepository,IPageDialogService dialogService) : base(gestureService,navigationService)
+        {
+            _twitchRepository = twitchRepository;
+            _dialogService = dialogService;
+            BackCommand = new DelegateCommand(() => TerminateAppliation());
+        }
+
+        #endregion
+
+        public override async void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+        {
+            base.OnNavigatedTo(e, viewModelState);
+
+            var games = await _twitchRepository.GetFeaturedGames();
+        }
+
+        private async void TerminateAppliation()
+        {
+            var exitComfirmed = await _dialogService.DisplayConfirmationDialogAsync("Do you want to exit from the application?","Are you sure?");
+            if (exitComfirmed)
+                Application.Current.Exit();
         }
     }
 }
