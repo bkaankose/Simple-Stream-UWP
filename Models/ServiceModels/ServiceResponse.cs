9ec7006b437c7fecee62e493e1d225dfe20ce289
Burak Kaan Köse <bkaankose@outlook.com>
2016-06-07 00:19:02 +0300

    ServiceResponse class is implemented for future client calls and Exception handling.

---
+++ Models/ServiceModels/ServiceResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Stream_UWP.Models.ServiceModels
+{
+    public class ServiceResponse
+    {
+        public Exception ExceptionContainer { get; set; }
+        public bool IsSuccess
+        {
+            get
+            {
+                return ExceptionContainer == null ? true : false;
+            }
+        }
+        public object Data { get; set; }
+    }
+}
