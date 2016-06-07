945a84eca2b85410e6fcdc0d0b994706d1af2d17
Burak Kaan Köse <bkaankose@outlook.com>
2016-06-07 21:13:05 +0300

    2 converters added.

---
+++ Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Simple_Stream_UWP.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if(value.GetType() == typeof(bool))
+                return ((bool)value) == true ? Visibility.Visible : Visibility.Collapsed;
+            else
+            {
+                Debug.WriteLine("Non boolean type in the boolean to visibility converter.");
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Converters/BooleanToVisibilityReverseConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Simple_Stream_UWP.Converters
+{
+    public class BooleanToVisibilityReverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value.GetType() == typeof(bool))
+                return ((bool)value) == true ? Visibility.Collapsed : Visibility.Visible;
+            else
+            {
+                Debug.WriteLine("Non boolean type in the boolean to visibility reverse converter.");
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
