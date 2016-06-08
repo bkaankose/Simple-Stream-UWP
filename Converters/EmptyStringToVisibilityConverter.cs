93467c4d03c228a3d54c7a87259ccbda4fe79d6e
Burak Kaan Köse <bkaankose@outlook.com>
2016-06-08 14:22:53 +0300

    Empty string to visibility converter implemented.

---
+++ Converters/EmptyStringToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Simple_Stream_UWP.Converters
+{
+    public class EmptyStringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return Visibility.Collapsed;
+
+            return value.ToString().Equals(string.Empty) ? Visibility.Collapsed : Visibility.Visible;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
