48b92f47ff06520913e3833a2fa1b65613cba19b
Burak Kaan Köse <bkaankose@outlook.com>
2016-06-10 20:26:21 +0300

    Token model implemented.

---
+++ Models/TwitchToken.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Stream_UWP.Models
+{
+    public class TwitchToken
+    {
+        [JsonProperty("token")]
+        public string ValidToken { get; set; }
+        [JsonProperty("sig")]
+        public string Sig { get; set; }
+        [JsonProperty("mobile_restricted")]
+        public bool IsMobileRestricted { get; set; }
+    }
+}
