1cb3ce71d5c832c2285233a61c9044f8d03a2158
Burak Kaan Köse <bkaankose@outlook.com>
2016-06-11 22:14:50 +0300

    New converter added.
    This converter will help Media Element to stretch its content or not.

---
+++ Converters/MediaElementStretchConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace Simple_Stream_UWP.Converters
+{
+    public class MediaElementStretchConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var val = (bool)value;
+            return val == true ? Stretch.Fill : Stretch.UniformToFill;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
