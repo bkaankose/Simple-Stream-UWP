ba46bc3725c26c3e1fb6ab105d198864734890d0
Burak Kaan Köse <bkaankose@outlook.com>
2016-06-11 22:16:24 +0300

    Bitrate model implemented for adaptive media source.

---
+++ Models/BitrateModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Stream_UWP.Models
+{
+    public class BitrateModel
+    {
+        public uint OriginalBitrate { get; set; }
+        public string BitrateName { get; set; }
+    }
+}
